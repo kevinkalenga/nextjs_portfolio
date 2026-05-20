db20cec3b0ac8723f408be371b1c7d363cd55e88
kevin <kevinkalenga10@gmail.com>
2026-05-20 12:58:57 +0200

    Featured Projects Section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -5,13 +5,13 @@ import { FaFacebookF, FaGithub, FaLinkedin } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <section className='py-28 container max-w-7xl mx-auto px-4'>
+    <section className='py-5 container max-w-7xl mx-auto px-4'>
       <div className='max-w-3xl mx-auto text-center'>
           <div className='flex flex-col items-center mb-4'>
              <Image src="/photo_kevin.jpg" alt="profile name" width={100} height={100} 
              className='rounded-full mb-4 w-32 h-32 object-cover ring-2 ring-primary'/>
           </div>
-          <h1 className='text-4xl md:text-3xl font-semibold mb-6'>Bonjour; Je m'appl <span className='text-primary'>Kevin Kalenga</span></h1>
+          <h1 className='text-4xl md:text-3xl font-semibold mb-6'>Bonjour, Je m'appl <span className='text-primary'>Kevin Kalenga</span></h1>
           <p className='text-xl md:text-2xl text-gray-600 dark:text-gray-300 mb-8'>Développeur full stack | Passionné d'UI/UX | Contributeur Open Source</p>
           <div className='flex justify-center space-x-4 mb-8'>
             <Link href="https://github.com/kevinkalenga" className='text-2xl text-gray-600 hover:text-primary dark:text-gray-300 transition-colors'>
