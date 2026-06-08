2946214bf82989b53231fd74130adc00d649373b
kevin <kevinkalenga10@gmail.com>
2026-06-08 18:13:09 +0200

    Fixed problem

--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,12 @@ export type Blog = {
    excerpt: string;
     readTime: string;
 };
+
+export type Project = {
+  title: string;
+  description: string;
+  image: string;
+  technologies: string[];
+  githubLink: string;
+  demoLink: string;
+};
